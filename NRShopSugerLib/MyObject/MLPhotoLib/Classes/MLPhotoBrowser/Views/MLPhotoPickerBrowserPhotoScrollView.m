0c98ba4988b2f2e45b4f585509ff0d4019cf96be
Gaojunwei <469445581@qq.com>
2016-09-20 16:53:48 +0800

    修复警告

--- NRShopSugerLib/MyObject/MLPhotoLib/Classes/MLPhotoBrowser/Views/MLPhotoPickerBrowserPhotoScrollView.m
+++ NRShopSugerLib/MyObject/MLPhotoLib/Classes/MLPhotoBrowser/Views/MLPhotoPickerBrowserPhotoScrollView.m
@@ -88,7 +88,7 @@
     }
 }
 
-- (void)actionSheet:(UIActionSheet *)actionSheet clickedButtonAtIndex:(long)buttonIndex{
+- (void)actionSheet:(UIActionSheet *)actionSheet clickedButtonAtIndex:(NSInteger)buttonIndex{
     if (buttonIndex == 0){
         if([UIImagePickerController isSourceTypeAvailable: UIImagePickerControllerSourceTypeSavedPhotosAlbum]) {
 
